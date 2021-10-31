9e4b21f6153747ae2f9ecd85f773dea8ee631bc1
Lê Huy <lehuylxag1995@gmail.com>
2021-10-31 12:04:13 +0700

    Initial page create, validation form, not process submit form

--- BlazorTodoList.ViewModel/TodoViewModel/RequestTodoCreate.cs
+++ BlazorTodoList.ViewModel/TodoViewModel/RequestTodoCreate.cs
@@ -1,6 +1,7 @@
 ﻿using BlazorTodoList.ViewModel.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,15 @@ namespace BlazorTodoList.ViewModel.TodoViewModel
     public class RequestTodoCreate
     {
         //public Guid Id { get; set; }
+        [Required]
+        [MaxLength(20)]
         public string NameTodo { get; set; }
-        public Priority Priority { get; set; }
-        public Status Status { get; set; }
-        //public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public Priority? Priority { get; set; }
+
+        public Status Status { get; set; } = Status.Open;
 
         public Guid? AssignId { get; set; }
-        //[ForeignKey("AssignId")]
-        //public User Assignee { get; set; }
     }
 }
---
+++ BlazorTodoListWasm/Pages/TodoCreate.razor.cs
@@ -0,0 +1,33 @@
+﻿using BlazorTodoList.ViewModel.Enums;
+using BlazorTodoList.ViewModel.TodoViewModel;
+using BlazorTodoList.ViewModel.UserViewModel;
+using BlazorTodoListWasm.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTodoListWasm.Pages
+{
+    public partial class TodoCreate
+    {
+        [Inject]
+        private IUserService _userService { get; set; }
+
+        private RequestTodoCreate _requestCreate = new RequestTodoCreate();
+        private string[] _listPriority = Enum.GetNames(typeof(Priority));
+        private List<AssigneeVm> _ListAssignee;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _ListAssignee = await _userService.GetAllAsync();
+        }
+
+        private async Task SubmitCreate(EditContext form)
+        {
+
+        }
+    }
+}
